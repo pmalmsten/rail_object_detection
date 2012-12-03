32e7e77673252d9eca955842742de4dbd65f2ce1
Paul Malmsten <pmalmsten@gmail.com>
2012-12-03 10:06:45 -0500

    Added --debug flag to extract_objects and extract_objects_rgb
    When --debug is present, extract_objects(_rgb) will save the intermediate
    point clouds generated during processing, including the downsampled
    original, the original minus large planes, and the planes that
    were extracted.

--- rail_pcl_object_segmentation/src/extract_objects.cpp
+++ rail_pcl_object_segmentation/src/extract_objects.cpp
@@ -68,27 +68,57 @@ int main(int argc, char** argv)
 {
   if (argc < 2)
   {
-    std::cerr << "Usage: extract_objects path/to/input.pcd" << std::endl;
+    std::cerr << "Usage: extract_objects [options] path/to/input.pcd" << std::endl;
+    std::cerr << " Options:" << std::endl;
+    std::cerr << "   --debug            Saves all intermediate point clouds to disk, including" << std::endl;
+    std::cerr << "                      the original minus planes and each extracted plane" << std::endl;
     std::cerr << " Extracted objects are saved to object_*.pcd in the" << std::endl << " current directory."
         << std::endl;
-    return -1;
+    return 1;
   }
+  
+  bool inDebugMode = (argc == 3 && strcmp(argv[1], "--debug") == 0);
+  pcl::PCDWriter writer;
 
   // Read input file
-  std::cout << "Reading '" << argv[1] << "'..." << std::endl;
+  std::cout << "Reading '" << argv[argc - 1] << "'..." << std::endl;
   pcl::PCDReader reader;
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
-  reader.read(argv[1], *cloud);
+  reader.read(argv[argc - 1], *cloud);
 
-  // Process
-  std::cout << "Extracting objects..." << std::endl;
-
+  // Down sample
   pcl::PointCloud<pcl::PointXYZ>::Ptr processed_cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(
       new pcl::PointCloud<pcl::PointXYZ>());
-  rail::DownsampleCloud<pcl::PointXYZ>(cloud, processed_cloud);
 
+  rail::DownsampleCloud<pcl::PointXYZ>(cloud, processed_cloud);
+  
+  if(inDebugMode) {
+      std::cout << "Writing downsampled cloud as 'input_downsampled_rgb.pcd'" << std::endl;
+      writer.write("input_downsampled_rgb.pcd", *processed_cloud);
+  }
+
+  // Remove planes
   std::vector<rail::DiscoveredPlanePtr> planes;
   rail::ExtractPlanes<pcl::PointXYZ>(processed_cloud, processed_cloud, planes);
+  
+  if(inDebugMode) {
+      // Dump original w/o planes
+      std::cout << "Writing input without large planes as 'input_minus_planes_rgb.pcd'" << std::endl;
+      writer.write("input_minus_planes_rgb.pcd", *processed_cloud);
+      
+      // Dump planes
+      unsigned int i = 0;
+      for(std::vector<rail::DiscoveredPlanePtr>::iterator it = planes.begin(); it != planes.end(); ++it) {
+          std::stringstream fileName;
+          fileName << "extracted_plane_" << i++ << "_rgb.pcd";
+          
+          std::cout << " Writing extracted plane to " << fileName.str() << std::endl;
+          writer.write(fileName.str(), (*(*it)).planeCloud);
+      }
+  }
+
+  // Extract objects
+  std::cout << "Extracting objects..." << std::endl;
 
   std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> objects;
   rail::ExtractObjectClouds<pcl::PointXYZ>(processed_cloud, objects);
@@ -96,12 +126,11 @@ int main(int argc, char** argv)
   std::cout << " Extracted " << objects.size() << " objects." << std::endl;
 
   // Save results
-  pcl::PCDWriter writer;
   int i = 0;
   for (std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr>::iterator it = objects.begin(); it != objects.end(); ++it)
   {
     std::stringstream fileName;
-    fileName << "object_" << i << ".pcd";
+    fileName << "object_" << i << "_rgb.pcd";
 
     std::cout << "Writing " << fileName.str() << std::endl;
 
--- rail_pcl_object_segmentation/src/extract_objects_rgb.cpp
+++ rail_pcl_object_segmentation/src/extract_objects_rgb.cpp
@@ -50,6 +50,7 @@
 
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <vector>
 
 #include <pcl/io/pcd_io.h>
@@ -69,29 +70,58 @@ int main(int argc, char** argv)
 {
   if (argc < 2)
   {
-    std::cerr << "Usage: extract_objects path/to/input.pcd" << std::endl;
+    std::cerr << "Usage: extract_objects_rgb [options] path/to/input.pcd" << std::endl;
+    std::cerr << " Options:" << std::endl;
+    std::cerr << "   --debug            Saves all intermediate point clouds to disk, including" << std::endl;
+    std::cerr << "                      the original minus planes and each extracted plane" << std::endl;
     std::cerr << " Input file must contain points of type PointXYZRGB." << std::endl;
     std::cerr << " Extracted objects are saved to object_*.pcd in the" << std::endl << " current directory."
         << std::endl;
-    return -1;
+    return 1;
   }
+  
+  bool inDebugMode = (argc == 3 && strcmp(argv[1], "--debug") == 0);
+  pcl::PCDWriter writer;
 
   // Read input file
-  std::cout << "Reading '" << argv[1] << "'..." << std::endl;
+  std::cout << "Reading '" << argv[argc - 1] << "'..." << std::endl;
   pcl::PCDReader reader;
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
-  reader.read(argv[1], *cloud);
+  reader.read(argv[argc - 1], *cloud);
 
-  // Process
-  std::cout << "Extracting objects..." << std::endl;
-
+  // Down sample
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr processed_cloud = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(
       new pcl::PointCloud<pcl::PointXYZRGB>());
 
   rail::DownsampleCloud<pcl::PointXYZRGB>(cloud, processed_cloud);
+  
+  if(inDebugMode) {
+      std::cout << "Writing downsampled cloud as 'input_downsampled_rgb.pcd'" << std::endl;
+      writer.write("input_downsampled_rgb.pcd", *processed_cloud);
+  }
 
+  // Remove planes
   std::vector<rail::DiscoveredPlanePtr> planes;
   rail::ExtractPlanes<pcl::PointXYZRGB>(processed_cloud, processed_cloud, planes);
+  
+  if(inDebugMode) {
+      // Dump original w/o planes
+      std::cout << "Writing input without large planes as 'input_minus_planes_rgb.pcd'" << std::endl;
+      writer.write("input_minus_planes_rgb.pcd", *processed_cloud);
+      
+      // Dump planes
+      unsigned int i = 0;
+      for(std::vector<rail::DiscoveredPlanePtr>::iterator it = planes.begin(); it != planes.end(); ++it) {
+          std::stringstream fileName;
+          fileName << "extracted_plane_" << i++ << "_rgb.pcd";
+          
+          std::cout << " Writing extracted plane to " << fileName.str() << std::endl;
+          writer.write(fileName.str(), (*(*it)).planeCloud);
+      }
+  }
+
+  // Extract objects
+  std::cout << "Extracting objects..." << std::endl;
 
   std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> objects;
   rail::ExtractObjectClouds<pcl::PointXYZRGB>(processed_cloud, objects);
@@ -99,7 +129,6 @@ int main(int argc, char** argv)
   std::cout << " Extracted " << objects.size() << " objects." << std::endl;
 
   // Save results
-  pcl::PCDWriter writer;
   int i = 0;
   for (std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>::iterator it = objects.begin(); it != objects.end(); ++it)
   {
